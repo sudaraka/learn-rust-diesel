616bf2980758dce965b2a8fc69f5f173613938a2
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2018-02-25 14:35:48 +0530

    Add library function to create new post record

--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,23 @@ use diesel::prelude::*;
 use dotenv::dotenv;
 use std::env;
 
+use self::models::NewPost;
+
 pub fn db_connection() -> SqliteConnection {
     dotenv().ok();
 
     let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
     SqliteConnection::establish(&db_url).expect(&format!("Error connecting to {}", db_url))
+}
+
+pub fn create_post(conn: &SqliteConnection, title: &str, body: &str) -> usize {
+    use schema::posts;
+
+    let new_post = NewPost { title, body };
+
+    diesel::insert_into(posts::table)
+        .values(&new_post)
+        .execute(conn)
+        .expect("Error saving new post")
 }
