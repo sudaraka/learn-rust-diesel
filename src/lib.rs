146f985ed4e5f9fb8c1ff986948c8180c3c44776
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2018-02-25 13:21:06 +0530

    Add library function to create and return database connection

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+#[macro_use]
+extern crate diesel;
+extern crate dotenv;
+
+use diesel::prelude::*;
+use dotenv::dotenv;
+use std::env;
+
+pub fn db_connection() -> SqliteConnection {
+    dotenv().ok();
+
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    SqliteConnection::establish(&db_url).expect(&format!("Error connecting to {}", db_url))
+}
